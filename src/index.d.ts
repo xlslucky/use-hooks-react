8d34e13e72dbe6ab33df81e1b400735b3d9ff1f7
xlslucky <xlslucky@gmail.com>
2019-08-04 20:35:55 +0800

    add index.d.ts

---
+++ src/index.d.ts
@@ -0,0 +1,9 @@
+export = useHooks;
+export as namespace useHooks;
+
+declare namespace useHooks {
+  function useModal(): void;
+  function usePage(): void;
+  function useResize(): void;
+  function useSearch(): void;
+}
--- src/index.js
+++ src/index.js
@@ -2,3 +2,12 @@ export { default as useModal } from './hooks/useModal'
 export { default as usePage } from './hooks/usePage'
 export { default as useResize } from './hooks/useResize'
 export { default as useSearch } from './hooks/useSearch'
+
+const useHooks = {
+  useModal,
+  usePage,
+  useResize,
+  useSearch,
+}
+
+export default useHooks
