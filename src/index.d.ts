cdac7d81cccfec0d730e17d9f47bd20e1bb8acd2
xlslucky <xlslucky@gmail.com>
2019-08-23 17:48:48 +0800

    update package.json

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,8 +2,12 @@ export = useHooks;
 export as namespace useHooks;
 
 declare namespace useHooks {
+  // visible, current, openModal, closeModal
   function useModal(): [];
+  // list, pageInfo, loading, setList, setPageInfo, setLoading
   function usePage(): [];
+  // attribute
   function useResize(): [];
+  // refresh, params, setRefresh, setParams
   function useSearch(initParams: object, initRefresh: boolean): [];
 }
