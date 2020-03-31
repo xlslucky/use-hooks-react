ab17d2e975eea8a72f72b3431c9e72aad11e409e
xlslucky <xlslucky@gmail.com>
2020-03-31 22:26:24 +0800

    update .d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,13 +1,30 @@
 export = useHooks;
 export as namespace useHooks;
 
+interface Attribute {
+  bodyClientWidth: number
+  bodyClientHeight: number
+  bodyOffsetWidth: number
+  bodyOffsetHeight: number
+  bodyScrollWidth: number
+  bodyScrollHeight: number
+  bodyScrollTop: number
+  bodyScrollLeft: number
+  windowScreenTop: number
+  windowScreenLeft: number
+  screenHeight: number
+  screenWidth: number
+  screenAvailHeight: number
+  screenAvailWidth: number
+}
+
 declare namespace useHooks {
   // visible, current, openModal, closeModal
-  function useModal(initItem: any, initVisible: boolean): [];
+  function useModal(initItem?: any, initVisible?: boolean): [boolean, any, Function, Function];
   // list, pageInfo, loading, setList, setPageInfo, setLoading
-  function usePage(): [];
+  function usePage(): [Array<any>, any, boolean, Function, Function, Function];
   // attribute
-  function useResize(): [];
+  function useResize(): [Attribute];
   // refresh, params, setRefresh, setParams
-  function useSearch(initParams: object, initRefresh: boolean): [];
+  function useSearch(initParams?: object, initRefresh?: boolean): [boolean, any, Function, Function];
 }
