0288c723e63e0f6efda67900cf42ec5c774e25d6
xlslucky <xlslucky@gmail.com>
2020-12-20 21:04:33 +0800

    update index.d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -25,15 +25,17 @@ interface BasicAttribute {
   bodyOffsetWidth: number
 }
 
+type StateAction<T> = React.Dispatch<React.SetStateAction<T>>
+
 declare namespace useHooks {
   // visible, current, openModal, closeModal
   function useModal<T>(initItem?: T, initVisible?: boolean): [boolean, T, (item?: T) => void, () => void];
   // list, pageInfo, loading, setList, setPageInfo, setLoading
-  function usePage<T, U = undefined>(): [T, U, boolean, (list: T) => void, (pageInfo: U) => void, (loading: boolean) => void];
+  function usePage<T, U = undefined>(): [T, U, boolean, StateAction<T>, StateAction<U>, StateAction<BooleanConstructor>];
   // attribute
   function useResize(): [Attribute];
   // attribute
   function useBasicResize(): BasicAttribute;
   // refresh, params, setRefresh, setParams
-  function useSearch<T>(initParams?: T, initRefresh?: boolean): [boolean, T, (refresh: boolean) => void, (params: T) => void];
+  function useSearch<T>(initParams?: T, initRefresh?: boolean): [boolean, T, StateAction<boolean>, StateAction<T>];
 }
